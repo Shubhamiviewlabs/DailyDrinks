1f3f05226933204c91f553059aa5da889bdb2218
Shubham Singh <shubham.singh@iviewlabs.net>
2021-02-26 13:35:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Index from "./components/index";
+function App() {
+  return (
+    <React.Fragment>
+      <Index />
+    </React.Fragment>
+  );
+}
+
+export default App;
